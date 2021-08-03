9285306d0a7dd4e9c2975c4f94a38d5d056f259c
monkjunior <vungocson998@gmail.com>
2021-08-03 08:49:34 +0700

    Add notfound to router

--- main.go
+++ main.go
@@ -15,10 +15,15 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 }
 
+func NotFound(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "%s Not Found!\n", r.URL.Path)
+}
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
-
+	router.GET("/notfound", NotFound)
 	http.ListenAndServe(":8080", router)
 }
