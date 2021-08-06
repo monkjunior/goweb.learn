3158b4c4009307207e2da7d6b3ad7c29325b5b26
monkjunior <vungocson998@gmail.com>
2021-08-06 16:31:22 +0700

    Wrote a draft code to parse POST signup form

--- controllers/users.go
+++ controllers/users.go
@@ -32,5 +32,9 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "This is a fake msg. Pretend the we created the user account.")
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, r.PostForm["email"])
+	fmt.Fprintln(w, r.PostForm["password"])
 }
