2119691ec46b78ba33c8db78eb6a9ff577391742
monkjunior <vungocson998@gmail.com>
2021-08-06 13:35:44 +0700

    Use panel to contain signup form

--- exp/main.go
+++ exp/main.go
@@ -1,41 +1,13 @@
 package main
 
-import (
-	"html/template"
-	"os"
-)
-
-type User struct {
-	Name string
-}
+import "fmt"
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
-	if err != nil {
-		panic(err)
-	}
-
-	data := User{
-		Name: "Monk Junior",
-	}
-
-	err = t.Execute(os.Stdout, data)
-	if err != nil {
-		panic(err)
-	}
-
-	data.Name = "Teddy Portal"
-
-	err = t.Execute(os.Stdout, data)
-	if err != nil {
-		panic(err)
-	}
-
-	// HTML template auto handle HTML encoding for us
-	data.Name = "<script>alert('hi')</script>"
-
-	err = t.Execute(os.Stdout, data)
-	if err != nil {
-		panic(err)
-	}
+	messages := make(chan string)
+	go func() { messages <- "hello" }()
+	go func() { messages <- "ping" }()
+	msg := <-messages
+	msg2 := <-messages
+	fmt.Println(msg)
+	fmt.Println(msg2)
 }
