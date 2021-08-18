acd22670d1b5cadef8101e5fd3e39c2d3dfc2bd4
monkjunior <vungocson998@gmail.com>
2021-08-18 07:22:26 +0700

    Created Gallery's validators

--- models/errors.go
+++ models/errors.go
@@ -4,7 +4,6 @@ import "strings"
 
 var (
 	ErrNotFound          modelError = "models: resource not found"
-	ErrIDInvalid         modelError = "models: ID provided was invalid"
 	ErrEmailRequired     modelError = "models: email address is required"
 	ErrEmailInvalid      modelError = "models: email provided was invalid"
 	ErrEmailIsTaken      modelError = "models: email address has already taken"
@@ -12,7 +11,11 @@ var (
 	ErrPasswordRequired  modelError = "models: password is required"
 	ErrPasswordTooShort  modelError = "models: password must be at least 8 charaters"
 	ErrRememberRequired  modelError = "models: remember is required"
-	ErrRememberTooShort  modelError = "models: remember token must be at least 32 bytes"
+	ErrTitleRequired     modelError = "models: title is required"
+
+	ErrIDInvalid        privateError = "models: ID provided was invalid"
+	ErrRememberTooShort privateError = "models: remember token must be at least 32 bytes"
+	ErrUserIDRequired   privateError = "models: userID is required"
 )
 
 type modelError string
@@ -26,4 +29,10 @@ func (e modelError) Public() string {
 	split := strings.Split(s, " ")
 	split[0] = strings.Title(split[0])
 	return strings.Join(split, " ")
+}
+
+type privateError string
+
+func (e privateError) Error() string {
+	return string(e)
 }
